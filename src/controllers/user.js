4df6779586f50fce63245ab004d670641c21bef2
Wickedlolz <iceshotgg.linux@gmail.com>
2022-10-23 09:42:56 +0300

    add login/register functionality

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,46 @@
 const router = require('express').Router();
 
+const userService = require('../services/user');
+const { mapErrors } = require('../utils/mapErrors');
+
+router.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await userService.register(email, password);
+        const token = await userService.createToken(user);
+
+        const result = {
+            email: user.email,
+            id: user._id,
+            accessToken: token,
+        };
+
+        res.status(201).json(result);
+    } catch (error) {
+        const errors = mapErrors(error);
+        res.status(400).json({ message: errors });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await userService.login(email, password);
+        const token = await userService.createToken(user);
+
+        const result = {
+            email: user.email,
+            id: user._id,
+            accessToken: token,
+        };
+
+        res.status(200).json(result);
+    } catch (error) {
+        const errors = mapErrors(error);
+        res.status(400).json({ message: errors });
+    }
+});
+
 module.exports = router;
